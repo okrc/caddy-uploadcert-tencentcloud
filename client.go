19f8478a3f94d3f1cbf4c5bd62b974f3c09a799c
okrc <okrc@hexo.dev>
2025-09-12 17:02:38 +0800

    feat: add GitHub Actions workflow and update certificate deployment logic
    Signed-off-by: okrc <okrc@hexo.dev>

--- client.go
+++ client.go
@@ -64,7 +64,7 @@ func (h *TencentCloudCertHandler) UploadCertificate(ctx context.Context, publicK
 	return nil
 }
 
-func (h *TencentCloudCertHandler) UpdateCertificateInstance(ctx context.Context, publicKey, privateKey, id string, DeployStatus *int64) error {
+func (h *TencentCloudCertHandler) UpdateCertificateInstance(ctx context.Context, publicKey, privateKey, id string, DeployRecordId *uint64) error {
 	requestData := UpdateCertificateInstanceRequest{
 		OldCertificateId: id,
 		ResourceTypes: []string{
@@ -92,7 +92,7 @@ func (h *TencentCloudCertHandler) UpdateCertificateInstance(ctx context.Context,
 	if response.Response.Error != nil {
 		return fmt.Errorf("%s", response.Response.Error.Message)
 	}
-	*DeployStatus = response.Response.DeployStatus
+	*DeployRecordId = response.Response.DeployRecordId
 	return nil
 }
 
